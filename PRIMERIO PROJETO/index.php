b13b2501abfa74721850e69433bdef98f582d425
GabiGiovanna <gabrielagiovanna3007@gmail.com>
2023-02-27 16:45:09 -0300

    Add files via upload

---
+++ PRIMERIO PROJETO/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>
+        <?php
+        echo "Ola Mundo";
+        ?>
+    </h1>
+
+    <h2>
+        2+1=
+        <?php
+        echo (2 + 1); ?>
+
+    </h2>
+
+    <h2>
+        5-2=
+        <?php
+        echo (5 - 2); ?>
+
+    </h2>
+
+    <h2>
+        4 / 2=
+        <?php
+        echo (4 / 2); ?>
+
+    </h2>
+
+    <h2>
+        2 * 2=
+        <?php
+        echo (2 * 2); ?>
+
+    </h2>
+
+</body>
+
+<style>
+
+body{
+    text-align: center;
+    background-color:pink;
+}
+
+
+</style>
+
+</html>
